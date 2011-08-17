50441b08f1a74b28643e27f952c5ff635a80af74
Michael P. Daugherty <mike@mpdaugherty.com>
2011-08-17 18:23:46 +0800

    Uncommented important lines

--- fabfile.py
+++ fabfile.py
@@ -9,13 +9,13 @@ env.key_filename = ['/Users/mpdaugherty/.ssh/mpd-aws-key.pem']
 BUDGET_ROOT = posixpath.normpath(posixpath.join(__file__.replace('\\', '/'), '../../')) + '/'
 
 def deploy():
-#    put(BUDGET_ROOT+'code')
+    put(BUDGET_ROOT+'code')
     run('[ -d /var/www/mpdaugherty-budget ] || mkdir /var/www/mpdaugherty-budget')
     with cd('/var/www/mpdaugherty-budget'):
         run('[ -d env ] || virtualenv --no-site-packages env')
         run('[ -d logs ] || ( mkdir logs && chmod g+w logs )')
-#        run('rm -rf code')
-#        run('mv ~/code code')
+        run('rm -rf code')
+        run('mv ~/code code')
         with prefix('source env/bin/activate'):
             run('pip install -r code/requirements.txt')
         sudo('mv code/config/apache.prod.conf /etc/apache2/sites-available/mpd-budget')
